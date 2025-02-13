4b6fa9e3a04b001466a404fcf0cf6ce47a77cacc
Arc <33088785+arcbtc@users.noreply.github.com>
2025-02-13 00:53:15 +0000

    Update migrations.py

--- migrations.py
+++ migrations.py
@@ -1,5 +1,35 @@
 async def m001_initial(db):
     """
+    Initial offlineshop tables.
+    """
+    await db.execute(
+        f"""
+        CREATE TABLE watchonly.shops (
+            id {db.serial_primary_key},
+            wallet TEXT NOT NULL,
+            method TEXT NOT NULL,
+            wordlist TEXT
+        );
+        """
+    )
+
+    await db.execute(
+        f"""
+        CREATE TABLE watchonly.items (
+            shop INTEGER NOT NULL REFERENCES {db.references_schema}shops (id),
+            id {db.serial_primary_key},
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            image TEXT, -- image/png;base64,...
+            enabled BOOLEAN NOT NULL DEFAULT true,
+            price {db.big_int} NOT NULL,
+            unit TEXT NOT NULL DEFAULT 'sat'
+        );
+        """
+    )
+
+async def m002_initial(db):
+    """
     Initial wallet table.
     """
     await db.execute(
